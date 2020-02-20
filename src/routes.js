8246b15acff7700bca5a0703e847e7d8ce78f209
Bruno Lagoa <miroldols@gmail.com>
2020-02-20 16:09:18 -0300

    Changes to be committed: 	modified:   src/routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,11 @@ const { Router } = require("express");
 const DevController = require("./controllers/DevController");
 const SearchController = require("./controllers/SearchController");
 
+// Tipos de parametros:
+// Query params
+// Route params
+// Body
+
 const routes = Router();
 
 routes.get("/", (req, res) => {
